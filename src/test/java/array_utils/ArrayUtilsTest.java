c518955bf48fbeece2fac07fcceacded706a0b35
Ville-Veikko Nieminen <vi-lle@hotmail.com>
2023-02-27 07:08:51 +0200

    Formatted code

--- src/test/java/array_utils/ArrayUtilsTest.java
+++ src/test/java/array_utils/ArrayUtilsTest.java
@@ -1,16 +1,13 @@
 package array_utils;
 
 import static org.junit.Assert.assertEquals;
-
 import java.util.Arrays;
-
 import org.junit.Test;
 
 public class ArrayUtilsTest {
 
 	@Test
 	public void testfindMinimumNumbersBetweenValuesUsingArray() {
-
 		int[] array = new int[] { -3, 1, 3, 10, 4 };
 		// 0 because of 3 and 4
 		assertEquals(0, ArrayUtils.findMinimumNumbersBetweenValues(array));
@@ -33,19 +30,18 @@ public class ArrayUtilsTest {
 
 	@Test
 	public void testfindMinimumNumbersBetweenValuesUsingIntegers() {
-
 		// 2 and 3 between 1 and 4
 		assertEquals(2, ArrayUtils.findMinimumNumbersBetweenValues(1, 4, 8, 16, 32));
-
+		
 		// 5, 6 and 7 between 4 and 8
 		assertEquals(3, ArrayUtils.findMinimumNumbersBetweenValues(4, 8, 16, 32, 64));
-
+		
 		// -2, -1, 0, 1 ,2, 3 and 4 between -3 and 5
 		assertEquals(7, ArrayUtils.findMinimumNumbersBetweenValues(-3, 5, 90, -100));
-
+		
 		// -2, -1, 0, 1 ,2, 3 and 4 between -3 and 5
 		assertEquals(7, ArrayUtils.findMinimumNumbersBetweenValues(5, 90, -3, -100));
-
+		
 		// 5, 6, 7 and 8 between 4 and 9
 		assertEquals(4, ArrayUtils.findMinimumNumbersBetweenValues(4, 9));
 	}
